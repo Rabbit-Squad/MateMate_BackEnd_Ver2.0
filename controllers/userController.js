301874342886314f3b6e79a6124b8654374173a4
dayeondayeon <dayeon2438@gmail.com>
2022-02-26 00:06:41 +0900

    Feat : 회원가입 구현

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const statusCode = require('../modules/status');
 const messageCode = require('../modules/message');
 const User = require('../models/user.js');
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 module.exports = {
@@ -32,5 +31,33 @@ module.exports = {
             message: messageCode.SIGN_IN_SUCCESS,
             userIdx : user[0][0].id
         });  
+    },
+
+    join : async (req, res) => {
+        const {
+            nickname,
+            email, 
+            pw
+        } = req.body;
+        
+        if (!nickname || !email || !pw) {
+            return res.status(statusCode.BAD_REQUEST).send(messageCode.MISS_DATA);
+        } // 정보 누락
+
+        if (!await User.checkEmail(email) || !await User.checkNickname(nickname)) {
+            return res.status(statusCode.ALREADY_EXIST).send(messageCode.ALREADY_EXIST);
+        } // 이미 존재하는 닉네임이나 이메일인 경우
+        
+        const hashedPw = await bcrypt.hash(pw, 12);
+        const user = await User.join(nickname, email, hashedPw);
+        if (user) {
+            return res.status(statusCode.SUCCESS).json({
+                code: statusCode.SUCCESS,
+                message: messageCode.SIGN_UP_SUCCESS,
+            }); 
+        } else {
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(messageCode.INTERNAL_SERVER_ERROR);
+        }
+       
     }
 }
--- models/user.js
+++ models/user.js
@@ -5,8 +5,44 @@ module.exports = {
     login : async (email) => {
         const sql = `SELECT * FROM User WHERE email="${email}"`;
         try {
-            const result = pool.queryParam(sql);
+            const result = await pool.queryParam(sql);
             return result;
+        } catch (err) {
+            throw err;
+        }
+    },
+    checkNickname : async(nickname) => {
+        const sql = `SELECT * From User WHERE User.nickname="${nickname}"`;
+        try {
+            const result = await pool.queryParam(sql);
+            if (result[0].length === 0) {
+                return true; // 중복 X
+            } else {
+                return false;
+            }
+        } catch (err) {
+            throw err;
+        }
+    },
+    checkEmail : async(email) => {
+        const sql = `SELECT * From User WHERE email="${email}"`;
+        try {
+            const result = await pool.queryParam(sql);
+            if (result[0].length === 0) {
+                return true; // 사용가능한 이메일(중복 X)
+            } 
+            else {
+                return false;
+            }
+        } catch (err) {
+            throw err;
+        }
+    },
+    join : async (nickname, email, pw) => {
+        const sql = `INSERT INTO User (nickname, email, pw) VALUES ('${nickname}', '${email}','${pw}')`;
+        try {
+            const result = await pool.queryParam(sql);
+            return true;
         } catch (err) {
             throw err;
         }
